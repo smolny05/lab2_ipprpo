b8df0f513d3269aa059c6c3128d4aa5f63574e68
mikita <nikita220085@gmail.com>
2025-12-16 10:52:04 +0300

    edited TemperatureConverter

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        double Celsius = 32;
+        double Fahrenheit = 41;
+        TemperatureConverter.CelsiusToFahrenheit(Celsius);
+        TemperatureConverter.FahrenheitToCelsius(Fahrenheit);
     }
 }
--- src/main/java/org/example/TemperatureConverter.java
+++ src/main/java/org/example/TemperatureConverter.java
@@ -1,4 +1,18 @@
 package org.example;
 
 public class TemperatureConverter {
+    public static double CelsiusToFahrenheit(double C) {
+        double fahrenheit;
+
+        fahrenheit = (C * 1.8) + 32;
+        System.out.println(" value of temperature in fahrenheit:" + fahrenheit);
+        return fahrenheit;
+    }
+    public static double FahrenheitToCelsius(double F) {
+        double celsius;
+
+        celsius = (F - 32) / 1.8;
+        System.out.println(" value of temperature in Celsius:" + celsius);
+        return celsius;
+    }
 }
